7be1ea9596c1a2b52313fc2d1529827d621aebd0
Tiago de Andrade Freire <tiago.freire@cubos.io>
2023-07-25 14:38:36 -0300

    feat: service retry settings

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## Changed
+
+- Service retry settings.
+
 ## [0.0.10] - 2023-07-24
 
 ### Added
--- node/index.ts
+++ node/index.ts
@@ -4,15 +4,20 @@ import { LRUCache, Service, method } from '@vtex/api'
 import { Clients } from './clients'
 import getSeller from './middlewares/getSeller'
 
-const TIMEOUT_MS = 800
+const TIMEOUT_MS = 3 * 1000
+const CONCURRENCY = 10
 const memoryCache = new LRUCache<string, never>({ max: 5000 })
 
 const clients: ClientsConfig<Clients> = {
   implementation: Clients,
   options: {
     default: {
+      exponentialTimeoutCoefficient: 2,
+      exponentialBackoffCoefficient: 2,
+      initialBackoffDelay: 50,
       retries: 2,
       timeout: TIMEOUT_MS,
+      concurrency: CONCURRENCY,
     },
     status: {
       memoryCache,
