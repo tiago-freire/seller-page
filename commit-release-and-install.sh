ab1190746d9cb0f0f0244d4909a684ecdaacfa1c
Tiago de Andrade Freire <tiago.freire@cubos.io>
2023-08-17 16:59:51 -0300

    feat: mobile seller banner

--- commit-release-and-install.sh
+++ commit-release-and-install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+set -e
+
 REPO=$(git remote get-url --push origin)
 echo $'\n=============== Commiting changes to repository '$REPO
 echo "Enter commit description: "
 read description
 git add .
 git commit -m "$description"
-git push origin main
+git push origin master
 
 echo $'\n=============== Releasing app'
 vtex switch ssesandbox04
